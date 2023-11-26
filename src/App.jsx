b6e0d67dec8fac8a3bf74fcf109985125690c2cb
kirubakaran22 <u.kirubakaran01@gmail.com>
2023-11-27 01:41:09 +0530

    Route changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 import RootLayout from "./Pages/RootLayout";
@@ -9,7 +9,7 @@ import { checkUserLoggedIn, logoutAction, queryClient } from "./util/http";
 import CartPage from "./Pages/Cart";
 import ErrorPage from "./Pages/Error";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <RootLayout />,
